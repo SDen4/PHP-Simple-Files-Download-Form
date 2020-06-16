6972b93a8f28d09345861ec48345551470389007
SDen4 <asw325@yandex.ru>
2020-06-16 23:52:55 +0500

    PHP-Simple-Files-Download-Form

--- index.php
+++ index.php
@@ -1,16 +1,24 @@
 <?php
 
-$login = 'admin';
-$password = '12345';
+// var_dump($_FILES);
 
-if(isset( $_POST['login']) && isset($_POST['password']) ) {
-    $mylogin = $_POST['login'];
-    $mypassword = $_POST['password'];
-
-    if($login == $mylogin && $password == $mypassword) {
-        include "./panel.php";
-        exit();
-    }
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    if($_FILES["picture"]["error"] == 0){
+        $tmpName = $_FILES["picture"]["tmp_name"];
+        if(move_uploaded_file($tmpName, "uploads/".time().".".pathinfo($_FILES["picture"]["name"])["extension"])){
+            echo "File saved";
+        }else{
+            echo "Can not save this file!!!";
+        };
+    }else{
+        echo "Upload error! Error code: ".$_FILES["picture"]["error"];
+    };
 };
+?>
 
-include "./form.php";
+<h1>Загрузка файла пользователем</h1>
+<form method="POST" enctype="multipart/form-data">
+    <input type="hidden" name="MAX_FILE_SIZE" value="50000000">
+    <input type="file" name="picture">
+    <button>Upload</button>
+</form>
--- readme.md
+++
@@ -1,9 +0,0 @@
-## Denis Skryabin
-
-##  Simple validation form on php:
-```sh
-$ git clone https://github.com/SDen4/PHP-Simple-Validation-Form.git
-$ cd PHP-Simple-Validation-Form
-$ npm i
-$ npm run dev
-```
